fa390abcbcd3679b8a8bd1ec5d1c1d7c89eac783
Julián Casaburi <juliancasaburi@gmail.com>
2020-03-03 12:06:17 -0300

    deploy

---
+++ static/pwa/precache-manifest.e58dd940f9cdfabbb8529c46862ea5d9.js
@@ -0,0 +1,40 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f707f067217d6ae67a900a15d88e0e",
+    "url": "/img/background.jpg"
+  },
+  {
+    "revision": "c4a11a77913de1cb94dec0ae3226702c",
+    "url": "/index.html"
+  },
+  {
+    "url": "/static/icons/icon_128x128.ee2678ee2de2d0955345a009430dae12.png"
+  },
+  {
+    "url": "/static/icons/icon_192x192.2e7a9f09eb4ce3a26b0d9ed7ea93e406.png"
+  },
+  {
+    "url": "/static/icons/icon_256x256.bfba6030220a6f9a2272a42de53917ea.png"
+  },
+  {
+    "url": "/static/icons/icon_384x384.60eff8c0760fa92894606fdc1fae7cf0.png"
+  },
+  {
+    "url": "/static/icons/icon_512x512.6475f27d549c0b523266292a0375d04e.png"
+  },
+  {
+    "url": "/static/icons/icon_96x96.111d9abf045a3e83af60c74a5da91e8c.png"
+  },
+  {
+    "url": "/static/images/e18977fd23c4850b464c07ab3d5e9f32.png"
+  },
+  {
+    "url": "/static/main.77491b1833aa13425d6f.css"
+  },
+  {
+    "url": "/static/main.77491b1833aa13425d6f.js"
+  },
+  {
+    "url": "/static/manifest.3e719e934d6a460cd1c09b830ada1059.json"
+  }
+]);
