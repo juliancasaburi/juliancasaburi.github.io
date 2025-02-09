3406c5c2291d0871b54b4f9c7bfca5ca92d076b6
Julian Casaburi <juliancasaburi@gmail.com>
2025-02-09 13:51:42 -0300

    chore: update projects

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
       <div className="group">
         <h1 className="title-bottom-line mb-5">Featured projects</h1>
 
-        <p>{`Here are some of the projects i've worked on (self-learning).`}</p>
+        <p>{`Here are some of the projects i've worked on.`}</p>
 
         <div className="mt-10 grid place-content-center gap-y-8 sm:grid-cols-2 sm:gap-x-3 sm:gap-y-5">
           {siteConfig.projects.map((pj, idx) => (
